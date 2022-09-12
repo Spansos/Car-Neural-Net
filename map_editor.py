c6b2d1b2b3017600500ccfdbbfb4db09d5ffb6b2
Spansos <92796536+Spansos@users.noreply.github.com>
2022-09-12 16:37:01 +0200

    Update map_editor.py

--- map_editor.py
+++ map_editor.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import sys
 
 class Point:
     def __init__(self, x: int, y: int) -> None:
@@ -13,7 +14,7 @@ class Line:
         self.color = col
         Line.lines.append(self)
     
-    def render(self, surf: pg.Surface) -> None:
+    def render(self, surf: pg.Surface, cam: pg.Vector2) -> None:
         pg.draw.line(surf, self.color, (self.p1.x, self.p1.y), (self.p2.x, self.p2.y))
     
     @classmethod
@@ -22,7 +23,18 @@ class Line:
             i.render(surf)
 
 def main():
-    pass
+    screen = pg.display.set_mode((800, 600))
+    cam = pg.Vector2((0,0))
+    while True:
+        for ev in pg.event.get():
+            if ev.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+        
+        
+        screen.fill((0,0,0))
+        Line.render_all(screen, cam)
+        pg.display.update()
 
 if __name__ == '__main__':
     main()
